7b9ad073b12de591eda6b539f87c7b038b0f9002
adarshgurjarc <158771139+adarshkumarc@users.noreply.github.com>
2024-02-05 07:20:47 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,1 +1,10 @@
-Use a Stream function and call a function 
+const fs = require("fs");
+const readStream = fs.createReadStream("./docs/blog3.txt",{encoding:'utf-8'});
+const writeStream=fs.createWriteStream('./docs/blog4.txt');
+readStream.on("data", (chunk) => {
+  console.log("------New chunk----");
+  console.log(chunk);
+  writeStream.write('\nNew chunk new line and new file\n');
+  writeStream.write(chunk);
+});
+readStream.pipe(writeStream);
